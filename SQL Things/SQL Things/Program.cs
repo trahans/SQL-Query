560dc972d5e12e22c3f2c5b8ca204251fee8caa7
unknown <christopher.grimm@TXWR3019QMW.Taxware.local>
2014-04-23 14:20:31 -0400

    Database connection established

--- SQL Things/SQL Things/Program.cs
+++ SQL Things/SQL Things/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace SQL_Things
 {
@@ -10,6 +11,27 @@ namespace SQL_Things
     {
         static void Main(string[] args)
         {
+            ServerConnect();
+        }
+
+        static void ServerConnect()
+        {
+            SqlConnection myConnection = new SqlConnection("server=localhost;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=TSE_Connector2; " +
+                                       "connection timeout=30");
+            try
+            {
+                myConnection.Open();
+                Console.WriteLine("Well done!");
+                Console.ReadLine();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("You failed!" + ex.Message);
+                Console.ReadLine();
+            }
+
         }
     }
 }
